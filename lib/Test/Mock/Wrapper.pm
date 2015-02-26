e5b662ffcbcc9b3f8180b85ccc9ddf4baf7a1e41
Dave Mueller <dmueller@liquidweb.com>
2015-02-26 09:10:28 -0500

    A few minor tweaks to layout

--- lib/Test/Mock/Wrapper.pm
+++ lib/Test/Mock/Wrapper.pm
@@ -7,8 +7,7 @@ use Test::Deep;
 use Test::More;
 use Clone qw(clone);
 use Scalar::Util qw(weaken isweak);
-require Test::Mock::Wrapped;
-require Test::Mock::Wrapped::Verify;
+require Test::Mock::Wrapper::Verify;
 
 # ABSTRACT: Flexible and prowerful class and object mocking library for perl
 
@@ -305,9 +304,41 @@ are I<chainable> to lend to more readable tests.
 
 sub verify {
     my($self, $method, %options) = @_;
-    return Test::Mock::Wrapped::Verify->new($method, $self->{__calls}{$method});    
+    return Test::Mock::Wrapper::Verify->new($method, $self->{__calls}{$method});    
 }
 
+package Test::Mock::Wrapped;
+use Carp;
+use Scalar::Util qw(weaken isweak);
+
+sub new {
+    my($proto, $controller, $object) = @_;
+    weaken($controller);
+    my $class = ref($proto) || $proto;
+    my $self = bless({__controller=>$controller, __object=>$object}, $class);
+    weaken($self->{__controller});
+    return $self;
+}
+
+sub AUTOLOAD {
+    my $self = shift;
+    my(@args) = @_;
+    $Test::Mock::Wrapped::AUTOLOAD=~m/::(\w+)$/;
+    my $method = $1;
+    if ($self->{__controller}->isMocked($method, @args)) {
+	return $self->{__controller}->_call($method, @args);
+    }
+    else {
+	if ($self->{__object}->can($method)) {
+	    unshift @_, $self->{__object}; 
+	    goto &{ ref($self->{__object}).'::'.$method };
+	}
+	else {
+	    my $pack = ref($self->{__object});
+	    croak qq{Can't locate object method "$method" via package "$pack"};
+	}
+    }
+}
 
 return 42;
 
--- lib/Test/Mock/Wrapper/Verify.pm
+++ lib/Test/Mock/Wrapper/Verify.pm
@@ -1,4 +1,4 @@
-package Test::Mock::Wrapped::Verify;
+package Test::Mock::Wrapper::Verify;
 
 use Test::Deep;
 use Test::More;
@@ -32,10 +32,24 @@ sub new {
     return bless({__calls=>$calls, method=>$method}, $class);
 }
 
+=head2 getCalls
+
+Returns an array of arrays representing all the calls to the mocked method which
+match any criteria added via a "with" call.
+
+=cut
+
 sub getCalls {
     my $self = shift;
     return clone($self->{__calls});
 }
+
+=head2 with(['some', 'args', ignore()])
+
+This returns a new verifier object with a call list which has been filtered using the
+supplied matcher. See L<Test::Deep> for information about matcher syntax.
+
+=cut
 
 sub with {
     my $self = shift;
@@ -44,32 +58,11 @@ sub with {
     return bless({__calls=>\@__calls, method=>$self->{method}}, ref($self));
 }
 
-sub never {
-    my $self = shift;
-    ok(scalar(@{ $self->{__calls} }) == 0,
-       "$self->{method} should never be called but was called ".scalar(@{ $self->{__calls} })." time".(scalar(@{ $self->{__calls} }) > 1 ? "s":'').".");
-    return $self;
-}
+=head2 exactly(N)
 
-sub once {
-    my $self = shift;
-    ok(scalar(@{ $self->{__calls} }) == 1, "$self->{method} should have been called once, but was called ".scalar(@{ $self->{__calls} })." times.");
-    return $self;
-}
-
-sub at_least {
-    my $self = shift;
-    my $times = shift;
-    ok(scalar(@{ $self->{__calls} }) >= $times, "$self->{method} only called ".scalar(@{ $self->{__calls} })." times, wanted at least $times\n");
-    return $self;
-}
-
-sub at_most {
-    my $self = shift;
-    my $times = shift;
-    ok(scalar(@{ $self->{__calls} }) <= $times, "$self->{method} called ".scalar(@{ $self->{__calls} })." times, wanted at most $times\n");
-    return $self;
-}
+Assert this method was called exactly N times. This is equivelent to
+    
+=cut
 
 sub exactly {
     my $self = shift;
@@ -78,4 +71,71 @@ sub exactly {
     return $self;
 }
 
+=head2 never
+
+Assert this method was never called. This is syntatic sugar, equivilent to
+    
+    $verify->exactly(0)
+
+=cut
+
+sub never {
+    my $self = shift;
+    ok(scalar(@{ $self->{__calls} }) == 0,
+       "$self->{method} should never be called but was called ".scalar(@{ $self->{__calls} })." time".(scalar(@{ $self->{__calls} }) > 1 ? "s":'').".");
+    return $self;
+}
+
+=head2 once
+
+Assert this method was called one time. This is syntatic sugar, equivilent to
+    
+    $verify->exactly(1)
+
+=cut
+
+sub once {
+    my $self = shift;
+    ok(scalar(@{ $self->{__calls} }) == 1, "$self->{method} should have been called once, but was called ".scalar(@{ $self->{__calls} })." times.");
+    return $self;
+}
+
+=head2 at_least(N)
+
+Assert this method was called at least N times.
+
+=cut
+
+sub at_least {
+    my $self = shift;
+    my $times = shift;
+    ok(scalar(@{ $self->{__calls} }) >= $times, "$self->{method} only called ".scalar(@{ $self->{__calls} })." times, wanted at least $times\n");
+    return $self;
+}
+
+=head2 at_most(N)
+
+Assert this method was called at most N times.
+
+=cut
+
+sub at_most {
+    my $self = shift;
+    my $times = shift;
+    ok(scalar(@{ $self->{__calls} }) <= $times, "$self->{method} called ".scalar(@{ $self->{__calls} })." times, wanted at most $times\n");
+    return $self;
+}
+
+
 return 42;
+
+=head1 AUTHOR
+
+  Dave Mueller <dave@perljedi.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Dave Mueller.
+
+This is free software; you can redistribute it and/or modify it under the
+same terms as the Perl 5 programming language system itself.
